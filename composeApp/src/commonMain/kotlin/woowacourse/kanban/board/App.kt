dce2e065bd8528aa76a75103f5351452ed76a1bd
katie0109 <0109katie@naver.com>
2026-03-05 10:38:40 +0900

    feat: 칸반보드 태스크 카드 생성 기능 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -2,42 +2,34 @@ package woowacourse.kanban.board
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import woowacourse.kanban.board.study.ButtonExample
-import woowacourse.kanban.board.study.ButtonLike
-import woowacourse.kanban.board.study.ButtonLove
-import woowacourse.kanban.board.study.ButtonSummit
-import woowacourse.kanban.board.study.IconExample
-import woowacourse.kanban.board.study.ImageExample
-import woowacourse.kanban.board.study.SimpleBox
-import woowacourse.kanban.board.study.TextExample
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.task.TaskCard
 
 @Composable
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 fun App() {
     MaterialTheme {
-        //var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(Color.White)
+                .padding(horizontal = 62.5.dp, vertical = 60.dp)
                 .safeContentPadding()
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            TextExample()
-            ImageExample()
-            IconExample()
-            ButtonExample()
-            ButtonSummit()
-            ButtonLove()
-            ButtonLike()
-            SimpleBox()
+            Row{
+                val tagNames = listOf("컴포넌트", "성능")
+                TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames, "다이노")
+            }
+
             }
         }
     }
