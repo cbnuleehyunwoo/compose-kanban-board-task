c3e974f1a1046e30f3535bd5b18778350f4ad342
cbnuLeehyunwoo <henow123@naver.com>
2026-03-07 17:42:07 +0900

    feat: TaskTitle 컴포저블 함수의 Preview 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/Title.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/Title.kt
@@ -5,10 +5,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.sp
 
+@Preview
 @Composable
-fun TaskTitle(title: String){
+fun TaskTitle(
+    @PreviewParameter(TitlePreviewParameterProvider::class)
+    title: String
+){
     Text(text = title,
         fontWeight = Bold,
         fontSize = 18.sp,
@@ -16,4 +23,11 @@ fun TaskTitle(title: String){
         maxLines = 1,
         overflow = TextOverflow.Ellipsis
         )
+}
+
+private class TitlePreviewParameterProvider : PreviewParameterProvider<String> {
+    override val values: Sequence<String> = sequenceOf(
+            "LazyColumn 컴포넌트 구현",
+            "너무너무 긴 제목은 한 줄까지만 노출되고 말줄임표로 처리됩니다."
+    )
 }
