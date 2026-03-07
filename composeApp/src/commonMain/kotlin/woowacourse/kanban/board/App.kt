7db9f30cdef134b0163b52e8a6c254aeba269252
cbnuLeehyunwoo <henow123@naver.com>
2026-03-07 16:57:29 +0900

    refactor: App에서 직접 테스크 카드를 보여주게 하는 대신, Card에서 Preview로 보여지게 하도록 리팩터링

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,20 +1,16 @@
 package woowacourse.kanban.board
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import kotlinx.serialization.builtins.ArraySerializer
-import woowacourse.kanban.board.task.TaskCard
+import woowacourse.kanban.board.task.Tag
+import woowacourse.kanban.board.task.TaskInfo
 
 @Composable
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
@@ -23,38 +19,15 @@ fun App() {
         Column(
             modifier = Modifier
                 .background(Color.White)
-                .padding(horizontal = 62.5.dp, vertical = 60.dp)
                 .safeContentPadding()
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(18.5.dp)
         ) {
-            Row (
-                horizontalArrangement = Arrangement.spacedBy(18.5.dp)
-            ) {
-                val tagNames01 = listOf("컴포넌트", "성능")
-                TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames01, "다이노")
-
-                val tagNames02 = listOf("컴포넌트", "성능")
-                TaskCard("LazyColumn 컴포넌트 구현", "", tagNames02, "다이노")
-
-                val tagNames03 = emptyList<String>()
-                TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames03, "다이노")
-
-                val tagNames04 = emptyList<String>()
-                TaskCard("LazyColumn 컴포넌트 구현", ".", tagNames04, "다이노")
-
-            }
-
-            Row () {
-                val tagNames05 = listOf("너무너무", "성능", "긴 태그", "최대로", "5자까지", "5개제한임")
-                TaskCard(
-                    "너무너무긴제목은 한줄까지만 노출됩니다.",
-                    "너무너무 긴 설명은 두 줄까지만 노출하고 말줄임표로 처리합니다. 너무너무 긴 설명은 두 줄까지만 노출하고 말줄임표로 처리합니다.",
-                    tagNames05,
-                    "너무너무너무 긴 담당자도 한 줄로 표시됩니다.",
-                )
-            }
-
+            TaskInfo(
+                title = "LazyColumn 컴포넌트 구현",
+                contents = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+                tags = listOf(Tag("컴포넌트"), Tag("성능")),
+                contactName = "다이노",
+            )
         }
     }
 }
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/Card.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/Card.kt
@@ -9,10 +9,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 
+@Preview(showBackground = true)
 @Composable
-fun TaskCard(title: String, contents: String? = null, tagNames: List<String>? = null, contactName: String? = null) {
+fun TaskCard(
+    @PreviewParameter(TaskCardPreviewParameterProvider::class)
+    taskInfo: TaskInfo,
+) {
     Column(
         modifier = Modifier
             .width(373.dp)
@@ -20,9 +27,43 @@ fun TaskCard(title: String, contents: String? = null, tagNames: List<String>? = 
             .padding(start = 22.5.dp, end = 22.5.dp, top = 22.5.dp, bottom = 22.5.dp),
         verticalArrangement = Arrangement.spacedBy(18.5.dp),
     ) {
-        TaskTitle(title)
-        contents?.let { TaskContents(contents) }
-        tagNames?.let { TaskTags(tagNames) }
-        contactName?.let { TaskContact(contactName) }
+        TaskTitle(taskInfo.title)
+        taskInfo.contents?.let { TaskContents(taskInfo.contents) }
+        taskInfo.tags?.let { TaskTags(taskInfo.tags) }
+        taskInfo.contactName?.let { TaskContact(taskInfo.contactName) }
     }
+}
+
+private class TaskCardPreviewParameterProvider: PreviewParameterProvider<TaskInfo> {
+    override val values: Sequence<TaskInfo> = sequenceOf(
+        TaskInfo(
+            title = "LazyColumn 컴포넌트 구현",
+            contents = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+            tags = listOf(Tag("컴포넌트"), Tag("성능")),
+            contactName = "다이노",
+        ),
+        TaskInfo(
+            title = "LazyColumn 컴포넌트 구현",
+            tags = listOf(Tag("컴포넌트"), Tag("성능")),
+            contactName = "다이노",
+        ),
+        TaskInfo(
+            title = "LazyColumn 컴포넌트 구현",
+            contents = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+            contactName = "다이노",
+        ),
+        TaskInfo(
+            title = "LazyColumn 컴포넌트 구현",
+            contactName = "다이노",
+        ),
+        TaskInfo(
+            title = "너무너무 긴 제목은 한 줄까지만 노출되고 말줄임표로 처리합니다.",
+            contents = "너무너무 긴 본문은 두 줄까지만 노출되고 말줄임표로 처리합니다. 너무너무 긴 본문은 두 줄까지만 노출되고 말줄임표로 처리합니다.",
+            tags = listOf(Tag("긴 태그"), Tag("최대로"), Tag("5자까지"), Tag("5개제한임"), Tag("줄임표가되...") ),
+            contactName = "너무너무너무 긴 담당자도 한 줄 까지만 노출되곷 말줄임표로 처리합니다.",
+        ),
+
+        )
+
+
 }
